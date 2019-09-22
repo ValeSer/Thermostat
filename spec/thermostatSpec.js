d280d87331ea0a208db3b55bd917f4049abc0a4a
Valeria Serci <balerty84@gmail.com>
2019-09-22 18:13:29 +0100

    Implement up method

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -8,8 +8,13 @@ describe('Thermostat', function() {
         thermostat = new Thermostat();
     });
 
-    it('thermostat starts at 20 degrees', function() {
+    it('starts at 20 degrees', function() {
         expect(thermostat.temperature).toEqual(20);
+    });
+    
+    it('can increase the temp', function(){
+        thermostat.up();
+        expect(thermostat.temperature).toEqual(21);
     });
 
 });
