5e52271869242402457fe312843a584007a1fd47
Valeria Serci <balerty84@gmail.com>
2019-09-22 22:24:29 +0100

    Add colors

--- src/interface.js
+++ src/interface.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
     // this code will be executed once when the page is finished loading
     var thermostate = new Thermostat();
+    update();
 
     function update() {
         $('#temperature').text(thermostate.temperature);
         $('#psm').text(thermostate.savingModeOn ? 'ON' : 'OFF');
         $('#usage').text(thermostate.usage());
+
+        // $('#usage').removeClass('high-usage');
+        // $('#usage').removeClass('medium-usage');
+        // $('#usage').removeClass('low-usage');
+        // $('#usage').addClass(thermostate.usage());
+
+        $('#usage').attr('class', thermostate.usage());
     }
 
     $('#temp_up').click(function() {
@@ -29,5 +37,4 @@ $(document).ready(function() {
     })
 
 
-    update();
 });
